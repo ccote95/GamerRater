89756493ad18413e4bf983c563a7743002efc200
Chad Cote <chadcote58@gmail.com>
2024-07-31 13:34:04 -0400

    Added logic for getting all games to the game_view, added imports to views/__init__.py, added urls, added auth.py

---
+++ raterapi/views/__init__.py
@@ -0,0 +1,2 @@
+from .games_view import GameView
+from .auth import login_user, register_user
--- raterproject/urls.py
+++ raterproject/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from raterapi.views.users import UserViewSet
+from raterapi.views import GameView,register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserViewSet, basename='user')
+router.register(r"games", GameView, basename='game')
 urlpatterns = [
     path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
 ]
 
