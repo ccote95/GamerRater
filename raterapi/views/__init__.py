b4b5d9d9efb904ad34dfbd7d8620ad02d6eda14e
Chad Cote <chadcote58@gmail.com>
2024-08-13 10:02:15 -0400

    Added the url for ratings

--- raterapi/views/__init__.py
+++ raterapi/views/__init__.py
@@ -2,3 +2,4 @@ from .games_view import GameView
 from .category_view import CategoryView
 from .auth import login_user, register_user
 from .reviews_view import ReviewView
+from .rating_view import RatingView
